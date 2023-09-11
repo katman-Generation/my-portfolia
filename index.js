7be6ab97d717c6f0d2e526a8444e472fc9fb3f16
katman-Generation <tkatema5@gmail.com>
2023-09-11 21:04:19 +0200

    adding phone css

---
+++ index.js
@@ -0,0 +1,8 @@
+function openNavigation() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNavigation() {
+  document.getElementById("mySidenav").style.width = "0";
+}
