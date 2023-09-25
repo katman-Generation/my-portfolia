3f7bb436729e1d10b3dbd38a3c16fe4e08b8c342
katman-Generation <tkatema5@gmail.com>
2023-09-25 04:37:10 +0200

    adding the javascipt

--- index.js
+++ index.js
@@ -6,3 +6,35 @@ function openNavigation() {
 function closeNavigation() {
   document.getElementById("mySidenav").style.width = "0";
 }
+function submitForm(){
+  const name = document.getElementById("name");
+  const email = document.getElementById("email")
+  const msg = document.getElementById("msg")
+
+
+  // This variable stores all the data.
+  let data = 
+  '\r Name: ' + name.value + ' \r\n ' + 
+  'Email: ' + email.value + ' \r\n ' + 
+  'Message: ' + msg.value;
+
+
+   // Convert the text to BLOB.
+  const textToBLOB = new Blob([data], { type: 'text/plain' });
+  const sFileName = 'formData.txt';	   // The file to save the data.
+
+  let newLink = document.createElement("a");
+  newLink.download = sFileName;
+
+  if (window.webkitURL != null) {
+    newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+  }
+  else {
+    newLink.href = window.URL.createObjectURL(textToBLOB);
+    newLink.style.display = "none";
+    document.body.appendChild(newLink);
+  }
+
+  newLink.click(); 
+  
+}
